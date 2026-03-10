545bdecd9302aea22267ad3fbe6534ab65579539
rojas-sebastian <rojasfsebastianf@gmail.com>
2026-03-09 22:15:07 -0500

    test(): set up intial tests

--- main.test.js
+++ main.test.js
@@ -1,5 +1,18 @@
 import main from "./main.js";
 
-test("Test 1", () => {
-    expect(main(14)).toBe(6);
+test("Test 1: 28", () => {
+    expect(main("011100")).toBe(7);
+});
+
+test("Test 1: 7", () => {
+    expect(main("111")).toBe(5);
+});
+
+test("Test 1: 7", () => {
+    expect(main("1111010101111")).toBe(22);
+});
+
+test("Test 1: 7", () => {
+    const binaryString = "1".repeat(400000);
+    expect(main(binaryString)).toBe(799999);
 });
