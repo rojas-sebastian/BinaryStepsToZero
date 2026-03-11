a03dfe36ca8d7491abe5310ac7bd58cc8764060a
rojas-sebastian <rojasfsebastianf@gmail.com>
2026-03-10 22:16:23 -0500

    chore(): fix logic to count correctly the number of steps and remove the insignificant bits

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-function main(A){
-    return 5
+function main(S){
+    // remove all zeros insignificant (to the left of the first '1'), before parsing to array
+    const binaryString = S.replace(/^0+/, "").split("");
+    let numberOperations = 0;
+    while(binaryString.length > 1 || binaryString.join("") !== "0"){
+        const isOddBit = binaryString.pop()
+        if(isOddBit === "1"){
+            binaryString.push("0");
+        }
+        numberOperations++;
+    }
+    return numberOperations;
 }
 
 export default main;
--- main.test.js
+++ main.test.js
@@ -4,15 +4,15 @@ test("Test 1: 28", () => {
     expect(main("011100")).toBe(7);
 });
 
-test("Test 1: 7", () => {
+test("Test 2: 7", () => {
     expect(main("111")).toBe(5);
 });
 
-test("Test 1: 7", () => {
+test("Test 3: 7855", () => {
     expect(main("1111010101111")).toBe(22);
 });
 
-test("Test 1: 7", () => {
+test("Test 4: too much 1", () => {
     const binaryString = "1".repeat(400000);
     expect(main(binaryString)).toBe(799999);
 });
